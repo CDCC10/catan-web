c7e03d2971c801776356ec5e60943b10cdbba3b7
CDCC10 <canizares.cdcc@gmail.com>
2025-07-30 04:05:51 -0400

    Initial commit

---
+++ server/src/main/java/com/cdcc/server/ServerSpringBootToCatanApplication.java
@@ -0,0 +1,13 @@
+package com.cdcc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSpringBootToCatanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSpringBootToCatanApplication.class, args);
+	}
+
+}
